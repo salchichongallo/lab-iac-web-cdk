3b06286c5982df01e984d174d3e51673fdab6e04
Jimmy Murillo <salchichongallo1997@gmail.com>
2025-06-01 10:43:39 -0500

    feat: add hello world lambda

--- lib/lab-iac-web-cdk-stack.ts
+++ lib/lab-iac-web-cdk-stack.ts
@@ -1,16 +1,31 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
 
 export class LabIacWebCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const myFunction = new lambda.Function(this, "HelloWorldFunction", {
+      functionName: "HelloWorldLambda",
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: "index.handler",
+      code: lambda.Code.fromInline(`
+        exports.handler = async function(event) {
+          return {
+            statusCode: 200,
+            body: JSON.stringify('Hello, World!'),
+          };
+        };
+        `),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'LabIacWebCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const myFunctionUrl = myFunction.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+    });
+
+    new cdk.CfnOutput(this, "myFunctionUrl", {
+      value: myFunctionUrl.url,
+    });
   }
 }

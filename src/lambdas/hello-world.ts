501732cd6378f295c5d4f778236f87f62125c071
Jimmy Murillo <salchichongallo1997@gmail.com>
2025-06-02 07:32:15 -0500

    refactor: update lambda message

--- src/lambdas/hello-world.ts
+++ src/lambdas/hello-world.ts
@@ -3,6 +3,6 @@ import { APIGatewayProxyResult } from "aws-lambda";
 export const handler = async (): Promise<APIGatewayProxyResult> => {
   return {
     statusCode: 200,
-    body: JSON.stringify("Hello, Lambda TS!"),
+    body: JSON.stringify("Hello, Lambda TS v2!"),
   };
 };

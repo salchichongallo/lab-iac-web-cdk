213e3d4f18766b016361d45e02747f9476480410
Jimmy Murillo <salchichongallo1997@gmail.com>
2025-06-02 07:06:17 -0500

    fix: compile lambda using esbuild

--- lib/lab-iac-web-cdk-stack.ts
+++ lib/lab-iac-web-cdk-stack.ts
@@ -7,12 +7,16 @@ export class LabIacWebCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const myFunction = new lambda.Function(this, "HelloWorldFunction", {
-      functionName: "HelloWorldLambda",
-      runtime: lambda.Runtime.NODEJS_20_X,
-      handler: "hello-world.handler",
-      code: lambda.Code.fromAsset(path.join(__dirname, "../src/lambdas")),
-    });
+    const myFunction = new cdk.aws_lambda_nodejs.NodejsFunction(
+      this,
+      "HelloWorldFunction",
+      {
+        functionName: "HelloWorldLambda",
+        entry: path.join(__dirname, "../src/lambdas/hello-world.ts"),
+        handler: "handler",
+        runtime: lambda.Runtime.NODEJS_20_X,
+      }
+    );
 
     const myFunctionUrl = myFunction.addFunctionUrl({
       authType: lambda.FunctionUrlAuthType.NONE,
